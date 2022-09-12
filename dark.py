Fix bugs related to zero rotation, flat scaling, weak ref spectra

dark.py
@@ -55,31 +55,38 @@ class Dark:
         dark = self._darks_by_exposure[exp_time]
         dark_hdu_l = fits.open(dark)
         dark_data = dark_hdu_l[0].data
-        result = in_data.astype(np.int32) - dark_data
+        result = np.asarray(in_data, dtype=np.float64) - np.asarray(dark_data, np.float64)
         dark_hdu_l.close()
         return result
 
     def _correct_scaled(self, in_data: npt.NDArray[Any], exp_time: float) -> npt.NDArray[Any]:
-        bias_hdu_l = fits.open(self._bias)
-        bias_data = bias_hdu_l[0].data
+        with fits.open(self._bias) as bias_hdu_l:
+            bias_data = np.asarray(bias_hdu_l[0].data, dtype=np.float64)
         dark_path = self._pick_dark(exp_time)
-        dark_hdu_l = fits.open(dark_path)
-        dark_data = dark_hdu_l[0].data - bias_data
-        dark_header = dark_hdu_l[0].header
-        scale = exp_time / float(dark_header['EXPTIME'])
-        dark_data = dark_data * scale
-        result = in_data.astype(np.int32) - dark_data - bias_data
-        dark_hdu_l.close()
-        bias_hdu_l.close()
+        if dark_path is not None:
+            with fits.open(dark_path) as dark_hdu_l:
+                dark_data = np.asarray(dark_hdu_l[0].data, dtype=np.float64) - bias_data
+                dark_header = dark_hdu_l[0].header
+            scale = exp_time / float(dark_header['EXPTIME'])
+            dark_data = dark_data * scale
+            result = np.asarray(in_data, dtype=np.float64) - dark_data - bias_data
+        else:
+            result = np.asarray(in_data, dtype=np.float64) - bias_data
         return result
 
-    def _pick_dark(self, exp_time: float) -> Path:
+    def _pick_dark(self, exp_time: float) -> Union[Path, None]:
         delta = None
         last_cand_time = None
         for cand_time in sorted(self._darks_by_exposure.keys()):
+            if cand_time < exp_time:
+                continue
+            if last_cand_time is None:
+                last_cand_time = cand_time
             new_delta = abs(exp_time - cand_time)
             if delta is not None and new_delta > delta:
                 break
             delta = new_delta
             last_cand_time = cand_time
+        if last_cand_time is None:
+            return None
         return self._darks_by_exposure[last_cand_time]

flat.py
@@ -30,7 +30,10 @@ def apply(param: FlatParam, input_files: Union[Path, Sequence[Path]], output_pat
         in_hdu_l = fits.open(input_file)
         header = in_hdu_l[0].header
         data = in_hdu_l[0].data
-        out_data = data[param.y_lo:param.y_hi, param.x_lo:param.x_hi]
+        if param.x_lo == 0 == param.x_hi == 0 and param.y_lo == param.y_hi:
+            out_data = data
+        else:
+            out_data = data[param.y_lo:param.y_hi, param.x_lo:param.x_hi]
         out_data = out_data / param.flat
         in_hdu_l.close()
         out_hdu = fits.PrimaryHDU(out_data, header)
@@ -62,7 +65,10 @@ def _compute_flat(flat_path: Path) -> FlatParam:
     data = flat_hdu_l[0].data
     x_lo, x_hi = _find_shortest_black(data[0, :])
     y_lo, y_hi = _find_shortest_black(data[:, 0])
-    cropped_data = data[y_lo:y_hi, x_lo:x_hi]
+    if x_lo == 0 and x_hi == 0 and y_lo == 0 and y_hi == 0:
+        cropped_data = data
+    else:
+        cropped_data = data[y_lo:y_hi, x_lo:x_hi]
     flat_hdu_l.close()
 
     xdata = np.arange(0, cropped_data.shape[1])
@@ -71,9 +77,5 @@ def _compute_flat(flat_path: Path) -> FlatParam:
         poly = Polynomial.fit(xdata, cropped_data[i, :], deg=4)
         # noinspection PyCallingNonCallable
         fitted[i, :] = poly(xdata)
-    cropped_data = cropped_data - fitted
-    min_d = np.min(cropped_data)
-    if min_d < 0:
-        cropped_data = cropped_data + min_d
-    cropped_data = cropped_data / np.max(cropped_data)
+    cropped_data = cropped_data / fitted
     return FlatParam(cropped_data, x_lo, y_lo, x_hi, y_hi)

slant.py
@@ -10,7 +10,8 @@ from scipy.signal import find_peaks
 
 
 def _find_next_peak(x_ref: Union[SupportsInt, SupportsFloat], x_data: npt.NDArray[Any]):
-    peaks, _ = find_peaks(x_data, prominence=2000)
+    stddev = np.std(x_data)
+    peaks, _ = find_peaks(x_data, prominence=stddev * 5)
     result = None
     last_delta = None
     for x in peaks:
@@ -27,9 +28,12 @@ def _move_row(in_row: npt.NDArray[Any], offset: float, x_0: int, out_row: npt.ND
     for x_target in range(0, out_row.shape[0]):
         x_src0 = int(floor(x_0 + x_target + offset))
         x_src1 = ceil(x_0 + x_target + offset)
-        w_0 = 1.0 - (x_0 + x_target + offset - x_src0)
-        w_1 = 1.0 - (x_src1 - x_0 - x_target - offset)
-        out_row[x_target] =  w_0 * in_row[x_src0] + w_1 * in_row[x_src1]
+        if x_src1 >= in_row.size or x_src0 > in_row.size:
+            out_row[x_target] = 0
+        else:
+            w_0 = 1.0 - (x_0 + x_target + offset - x_src0)
+            w_1 = 1.0 - (x_src1 - x_0 - x_target - offset)
+            out_row[x_target] =  w_0 * in_row[x_src0] + w_1 * in_row[x_src1]
 
 
 class Slant: